a9974ce1ede42323b87e15ddfa0e8f479a22b402
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-14 17:17:07 -0300

    :sparkles: feat: criando model genérico"MongoModel"

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,39 @@
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj:T):Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read():Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async readOne(id:string):Promise<T | null> {
+    if (!isValidObjectId(id)) throw Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOne({ id });
+  }
+
+  public async update(id:string, obj:T): Promise<T | null> {
+    if (!isValidObjectId(id)) throw Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndUpdate(
+      { id },
+      { ...obj } as UpdateQuery<T>,
+    );
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) throw Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndDelete({ id });
+  }
+}
+
+export default MongoModel;
