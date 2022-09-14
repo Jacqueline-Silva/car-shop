997778beb678c488d663d6c7cf751162ce1fe6c2
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-14 17:24:26 -0300

    :sparkles: feat: implementando o "index.ts" e adicionando a rota ao "app.ts"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRoute from './routes/cars';
 
 const app = express();
+app.use(express.json());
+
+app.use('/cars', carRoute);
+
+app.use(errorHandler);
 
 export default app;
