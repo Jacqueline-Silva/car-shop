97ae06e36aedb602b2c3c2115ab03c9dbb55cfcb
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 15:35:09 -0300

    :test_tube: test: camada controller - Cars: métodos "read" e "readOne"

--- src/tests/unit/controllers/cars.test.ts
+++ src/tests/unit/controllers/cars.test.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 import CarsModel from '../../../models/CarsModel';
 import CarsService from '../../../services/Cars';
 import CarsController from '../../../controllers/Cars';
-import { carsMock, carsMockWithID } from '../../mocks/carsMock';
+import { carsMock, carsMockWithID, idCarsMock } from '../../mocks/carsMock';
 
 describe('Cars Controller', () => {
   const carsModel = new CarsModel();
@@ -16,6 +16,8 @@ describe('Cars Controller', () => {
 
   before(() => {
     sinon.stub(carsService, 'create').resolves(carsMockWithID);
+    sinon.stub(carsService, 'read').resolves([carsMockWithID]);
+    sinon.stub(carsService, 'readOne').resolves(carsMockWithID);
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
@@ -25,11 +27,30 @@ describe('Cars Controller', () => {
     sinon.restore();
   });
 
-  describe('Verifica se a função "create"', () => {
-    it('Retorna os dados do carro cadastrado', async () => {
+  describe('Verifica se o método "create"', () => {
+    it('Retorna status 201 com os dados do carro cadastrado', async () => {
       req.body = carsMock;
       await carsController.create(req, res);
       expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carsMockWithID)).to.be.true;
+    });
+  });
+
+  describe('Verifica se o método "read"', () => {
+    it('Retorna status 200 e a lista com todos os carros cadastrados', async () => {
+      await carsController.read(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith([carsMockWithID])).to.be.true;
+    });
+  });
+
+  describe('Verifica se o método "readOne"', () => {
+    it('Retorna status 200 e os dados do carro selecionado', async () => {
+      req.params = { id: idCarsMock };
+      await carsController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(carsMockWithID)).to.be.true;
     });
   });
