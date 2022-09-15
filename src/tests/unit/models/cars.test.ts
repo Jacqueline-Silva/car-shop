5a67f2ecf0517147bb19cdba6ce240de7fd6cae6
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 15:08:37 -0300

    :test_tube: test: camada model - CarsModel: métodos "read" e "readOne"

--- src/tests/unit/models/cars.test.ts
+++ src/tests/unit/models/cars.test.ts
@@ -2,24 +2,50 @@ import * as sinon from 'sinon';
 import { Model } from 'mongoose';
 import { expect } from 'chai';
 import CarsModel from '../../../models/CarsModel';
-import { carsMock, carsMockWithID } from '../../mocks/carsMock';
+import { carsMock, carsMockWithID, idCarsMock } from '../../mocks/carsMock';
 
 describe('Cars Model', () => {
   const carsModel = new CarsModel();
 
   before(() => {
     sinon.stub(Model, 'create').resolves(carsMockWithID);
+    sinon.stub(Model, 'find').resolves([carsMockWithID]);
+    sinon.stub(Model, 'findOne').resolves(carsMockWithID);
   });
 
   after(() => {
     sinon.restore();
   });
 
-  describe('Verifica a função "create"', () => {
+  describe('Verifica o método "create"', () => {
     it('Sucesso ao cadastrar um novo carro', async () => {
       const newCars = await carsModel.create(carsMock);
       
       expect(newCars).to.be.deep.equal(carsMockWithID);
+    });
+  });
+
+  describe('Verifica o método "read"', () => {
+    it('Sucesso ao exibir a lista de carros cadastrados', async () => {
+      const allLens = await carsModel.read();
+
+      expect(allLens).to.be.deep.equal([carsMockWithID]);
+    });
+  });
+
+  describe('Verifica a função "readOne"', () => {
+    it('Sucesso ao procurar o carro pelo ID', async () => {
+      const carFound = await carsModel.readOne(idCarsMock);
+
+      expect(carFound).to.be.deep.equal(carsMockWithID);
+    });
+
+    it('Falha ao procurar o carro pelo ID', async () => {
+      try {
+				await carsModel.readOne('123ERRADO');
+			} catch (error: any) {
+				expect(error.message).to.be.eq('InvalidMongoId');
+			}
     });
   });
 });
