2c6c09bf750f498498ae27e86e3a081ef5546f1b
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 15:22:27 -0300

    :test_tube: test: camada service - Cars: métodos "read" e "readOne"

--- src/tests/unit/services/cars.test.ts
+++ src/tests/unit/services/cars.test.ts
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
 import CarsModel from '../../../models/CarsModel';
 import CarsService from '../../../services/Cars';
-import { carsMock, carsMockWithID } from '../../mocks/carsMock';
+import { carsMock, carsMockWithID, idCarsMock } from '../../mocks/carsMock';
 
 describe('Cars Service', () => {
 	const carsModel = new CarsModel();
@@ -11,13 +12,17 @@ describe('Cars Service', () => {
 
 	before(() => {
 		sinon.stub(carsModel, 'create').resolves(carsMockWithID);
+		sinon.stub(carsModel, 'read').resolves([carsMockWithID]);
+    sinon.stub(carsModel, 'readOne')
+		.onCall(0).resolves(carsMockWithID)
+		.onCall(1).resolves(null);
   });
 
 	after(() => {
 		sinon.restore();
 	});
 
-	describe('Verifica se a função "create"', () => {
+	describe('Verifica se o método "create"', () => {
 		it('Cadastra com sucesso um novo carro', async () => {
 			const results = await carsService.create(carsMock);
 
@@ -32,4 +37,29 @@ describe('Cars Service', () => {
 			}
 		});
 	});
+
+	describe('Verifica se o método "read"', () => {
+		it('Retorna com sucesso a lista de carros cadastrados', async () => {
+			const cars = await carsService.read();
+
+			expect(cars).to.be.deep.equal([carsMockWithID]);
+		});
+	});
+
+	describe('Verifica se o método "readOne"', () => {
+		it('Retorna com sucesso os dados do carro cadastrado', async () => {
+			const carId = await carsService.readOne(idCarsMock);
+
+			expect(carId).to.be.deep.equal(carsMockWithID);
+		});
+
+		it('Retorna um erro ao não passar o ID correto', async () => {
+			try {
+				await carsService.readOne('idIncorrect');
+			} catch (error:any) {
+				expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+			}
+		});
+	});
+
 });
