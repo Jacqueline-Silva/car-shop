7277fdb5795da678adb7cda4a63253e760bf350f
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 14:50:33 -0300

    :sparkles: feat: criando a rota GET /cars/:id

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -13,4 +13,6 @@ carRoute.post('/', (req, res) => carController.create(req, res));
 
 carRoute.get('/', (req, res) => carController.read(req, res));
 
+carRoute.get('/:id', (req, res) => carController.readOne(req, res));
+
 export default carRoute;
