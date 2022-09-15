ab07c544a0af5e29788df78c00f0cce32c89517e
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 19:51:29 -0300

    :sparkles: feat: criando interface "IMotorcycle" a partir da "IVehicle"

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+const categories = ['Street', 'Custom', 'Trail'] as const;
+
+export const motorcycleZodSchema = vehicleZodSchema.extend({
+  category: z.enum(categories),
+  engineCapacity: z.number().int().lte(2500),
+});
+
+export type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
+
+/**
+ * REF: https://www.npmjs.com/package/zod#strings
+ */
