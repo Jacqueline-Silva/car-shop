7c4aca3509676474f31fbaabb98abfbf18cae444
Jacqueline Silva <jacque.sx@hotmail.com>
2022-09-15 18:58:00 -0300

    :test_tube: test: camada controller - Cars: métodos "update" e "delete"

--- src/tests/unit/controllers/cars.test.ts
+++ src/tests/unit/controllers/cars.test.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 import CarsModel from '../../../models/CarsModel';
 import CarsService from '../../../services/Cars';
 import CarsController from '../../../controllers/Cars';
-import { carsMock, carsMockWithID, idCarsMock } from '../../mocks/carsMock';
+import { carsMock, carsMockWithID, idCarsMock, newCarsMock, newCarsMockWithID } from '../../mocks/carsMock';
 
 describe('Cars Controller', () => {
   const carsModel = new CarsModel();
@@ -18,9 +18,12 @@ describe('Cars Controller', () => {
     sinon.stub(carsService, 'create').resolves(carsMockWithID);
     sinon.stub(carsService, 'read').resolves([carsMockWithID]);
     sinon.stub(carsService, 'readOne').resolves(carsMockWithID);
+    sinon.stub(carsService, 'update').resolves(newCarsMockWithID);
+    sinon.stub(carsService, 'delete').resolves(carsMockWithID);
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
+    res.sendStatus = sinon.stub().returns(res);
   });
 
   after(() => {
@@ -52,6 +55,26 @@ describe('Cars Controller', () => {
 
       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(carsMockWithID)).to.be.true;
+    });
+  });
+
+  describe('Verifica se o método "update"', () => {
+    it('Retorna status 200 e os dados do carro atualizado', async () => {
+      req.params = { id: idCarsMock };
+      req.body = newCarsMock;
+      await carsController.update(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(newCarsMockWithID)).to.be.true;
+    });
+  });
+
+  describe('Verifica se o método "delete"', () => {
+    it('Retorna status 204', async () => {
+      req.params = { id: idCarsMock };
+      await carsController.delete(req, res);
+
+      expect((res.sendStatus as sinon.SinonStub).calledWith(204)).to.be.true;
     });
   });
 });
